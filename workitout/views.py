ce9b91ef5cd7cfd3fcf5ee02b3e04d9f00052198
Cormac <2375400m@student.gla.ac.uk>
2020-03-10 20:09:17 +0000

    incomplete version

--- workitout/views.py
+++ workitout/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+
+    return render(request, 'rango/home.html')
+
+def create-workout(request):
+
+    return render(request, 'rango/create-workout.html')
+
+def search(request):
+
+    return render(request, 'rango/search.html')
+    
+def user-page(request):
+
+    return render(request, 'rango/user-page.html')
