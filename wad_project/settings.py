423897703cc2d6746f28fea1e78f74b1b0a91528
Cormac Muir <2375400m@student.gla.ac.uk>
2020-04-03 13:08:17 +0100

    another static file change for hosting

--- wad_project/settings.py
+++ wad_project/settings.py
@@ -20,7 +20,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Path to static directory from STATIC_DIR
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = "/home/workitout/wad_project/static"
+STATIC_ROOT = '/home/workitout/wad_project/assets/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
