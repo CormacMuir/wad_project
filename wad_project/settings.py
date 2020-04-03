c3bf6c4feb1462e3b975f7dfc156a32df12f76dd
Cormac Muir <2375400m@student.gla.ac.uk>
2020-04-03 13:56:15 +0100

    trying to get static images showing on Panywhere

--- wad_project/settings.py
+++ wad_project/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
