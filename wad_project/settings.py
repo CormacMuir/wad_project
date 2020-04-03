e77b930a1d35e1d486bee95ab9d44e51e39adca6
Cormac Muir <2375400m@student.gla.ac.uk>
2020-04-03 15:47:35 +0100

    web static files

--- wad_project/settings.py
+++ wad_project/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-#STATICFILES_DIRS = [STATIC_DIR, ]
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = MEDIA_DIR
